099809f470fe0403880ef9158b573a2011aa074c
Yunus <Yunus0261@gmail.com>
2023-11-20 16:51:18 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 // next.config.js
 module.exports = {
   reactStrictMode: true,
-  output: "out", // ya da istediğiniz bir klasör adı
-  experimental: {
-    images: {
-      loader: "imgix",
-      path: "",
-    },
-  },
+  output: "out", // veya istediğiniz bir klasör adı
 };
