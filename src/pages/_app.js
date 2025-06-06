9124fe95f1cc9fac1bbbd0c992618c8146893691
yunus.genc <yunus.genc@nscsoft.com>
2025-06-06 04:18:15 +0300

    added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import Layout from "../../component/layout/layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+    </>
   );
 }
