3343fe1d4c1d4f2555cbcb843c5007f1dee62cbb
yunus.genc <yunus.genc@nscsoft.com>
2025-06-06 04:24:04 +0300

    added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import Layout from "../../component/layout/layout";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </Layout>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
